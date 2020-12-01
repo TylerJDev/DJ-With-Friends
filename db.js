fdcd91e8a8253d932111a1ad5b5e36a19225e815
Tyler <tylerjdev@gmail.com>
2020-12-01 17:43:01 -0500

    Fixed cypress issue

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: process.env.VUE_APP_apiKey,
   authDomain: process.env.VUE_APP_authDomain,
   databaseURL: process.env.VUE_APP_databaseURL,
-  projectId: process.env.VUE_APP_projectId,
+  projectId: process.env.VUE_APP_projectId || 'empty',
   storageBucket: process.env.VUE_APP_storageBucket,
   messagingSenderId: process.env.VUE_APP_messagingSenderId,
   appId: process.env.VUE_APP_appId,
